d53a102a656e8cc175a52008bfe8e76b51eae191
anonymous4reg <anonymous4reg@163.com>
2021-03-12 17:19:23 +0800

    sync

--- Mfile/FFT_and_SymmetricComponent/fft_symmetric_component.m
+++ Mfile/FFT_and_SymmetricComponent/fft_symmetric_component.m
@@ -1,4 +1,4 @@
-function [freq_ax, mag_ax, ang_ax] = fft_simple(input_sig, ...
+function [freq_ax, mag_ax, ang_ax] = fft_symmetric_component(input_sig, ...
 	fs, signal_length)
 %UNTITLED2 Summary of this function goes here
 %   fs: sampling frequency
@@ -6,10 +6,14 @@ function [freq_ax, mag_ax, ang_ax] = fft_simple(input_sig, ...
 %	t_span: time span
 %	signal_length: t_span * fs
 
+a = exp(1.0i * 2 * pi / 3);
+Abc2Pn0Mat = (1/3) .* [1  a  a^2; 1  a^2  a; 1  1  1];
+
 [row, col] = size(input_sig);
 ts = 1/fs;	% sampling period
 % signal_length = t_span * fs;  % lenght of sig
 t_span = signal_length / fs;
+
 
 raw_fft = fft(input_sig, signal_length, 1);
 
@@ -21,11 +25,20 @@ spectrum_single_mag(2:end - 1) = 2 .* spectrum_dual_mag(2:end - 1);  %
 spectrum_single_mag = spectrum_single_mag(1:(signal_length / 2), :);
 spectrum_single_angle = spectrum_dual_angle(1:(signal_length / 2), :);
 
+% why -1.0i or 1.0i
+spectrum_single_complex = spectrum_single_mag .* exp(-1.0i .* ...
+	spectrum_single_angle);
+
+spectrum_pn0_complex = (Abc2Pn0Mat * spectrum_single_complex')';
+
+spectrum_pn0_mag = abs(spectrum_pn0_complex);
+spectrum_pn0_angle = angle(spectrum_pn0_complex);
+
 freq_ax = 0 : (1/t_span) : fs/2;
 freq_ax = freq_ax(1 : end - 1)';
 
-mag_ax = spectrum_single_mag;
-ang_ax = spectrum_single_angle .* (abs(mag_ax) >= 1e-10);
+mag_ax = spectrum_pn0_mag;
+ang_ax = spectrum_pn0_angle .* (abs(mag_ax) >= 1e-10);
 
 end
 
--- Mfile/FFT_and_SymmetricComponent/pos_neg_fft_demo.m
+++ Mfile/FFT_and_SymmetricComponent/pos_neg_fft_demo.m
@@ -1,6 +1,8 @@
 % clear;clc
-PosThreePhaseGen = @(amp, freq, t) amp.*[sin(2*pi*freq.*t);  sin(2*pi*freq.*t - 2*pi/3); sin(2*pi*freq.*t + 2*pi/3)]';
-NegThreePhaseGen = @(amp, freq, t) amp.*[sin(2*pi*freq.*t);  sin(2*pi*freq.*t + 2*pi/3); sin(2*pi*freq.*t - 2*pi/3)]';
+PosThreePhaseGen = @(amp, freq, t) amp .* [cos(2*pi*freq.*t);  cos(2*pi*freq.*t - 2*pi/3); cos(2*pi*freq.*t + 2*pi/3)]';
+NegThreePhaseGen = @(amp, freq, t) amp .* [cos(2*pi*freq.*t);  cos(2*pi*freq.*t + 2*pi/3); cos(2*pi*freq.*t - 2*pi/3)]';
+ZeroThreePhaseGen = @(amp, freq, t, phase) amp .* ...
+    [cos(2*pi*freq.*t - phase); cos(2*pi*freq.*t - phase); cos(2*pi*freq.*t - phase)]';
 a = exp(1j * 2 * pi / 3);
 Abc2Pn0Mat = (1/3) .* [1  a  a^2; 1  a^2  a; 1  1  1];
 
@@ -15,13 +17,19 @@ t = (0:L-1)*T;  % time vector;
 
 Pos50Hz = PosThreePhaseGen(1, 50, t);
 Neg58Hz = NegThreePhaseGen(0.5, 58, t);
-SigSum = Pos50Hz;
+Zero70Hz = ZeroThreePhaseGen(0.75, 70, t, pi/6);
+SigSum = Pos50Hz + Neg58Hz + Zero70Hz;
 
-SigPN0 = Abc2Pn0Mat * SigSum';
-SigPN0 = SigPN0';
+[fx, mx, ax] = fft_symmetric_component(SigSum, Fs, L);
+% [fx, mx, ax] = fft_simple(SigSum, Fs, L);
 
-
-[fx, mx, ax] = fft_simple(Pos50Hz(:, 1) + Neg58Hz(:, 1), Fs, Tspan);
-plot(fx, mx, 'linewidth', 1.8)
-grid on
+subplot(2, 1, 1)
+stem(fx, mx, 'linewidth', 1.8)
 axis([0, 100, -0.1, 1.2])
+subplot(2, 1, 2)
+plot(fx, ax * 180 / pi, 'linewidth', 1.8)
+axis([0, 100, -190, 190])
+
+% plot(fx, mx(:, 1), 'linewidth', 1.8)
+% grid on
+% axis([0, 100, -0.1, 1.2])
