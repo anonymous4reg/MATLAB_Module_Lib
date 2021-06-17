02339c46586089dd6ecb5dff63beb87a495f9565
anonymous4reg <anonymous4reg@163.com>
2021-06-17 17:20:06 +0800

    sync

---
+++ Examples/transformer_config_verification/TransformerConfigTest_r2016b.m
@@ -0,0 +1,9 @@
+clear;clc
+Noloadloss = 100e3;  %100kW
+Noloadcur = 0.2e-2;
+Loadloss_h2m = 540e3;
+Loadloss_h2l = 161.15e3;
+Loadloss_m2l = 158.43e3;
+SC_imp_h2m = 0.18;
+SC_imp_h2l = 0.58;
+SC_imp_m2l = 0.38;
